4f9335c3395880feec77b4be8e54ac3078698ba5
Nguyen Huy Hoang <nhhoang2435@clc.fitus.edu.vn>
2026-04-12 10:06:53 +0700

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        int ans = 0;
+        unordered_set<int> mp(nums.begin(), nums.end());
+
+        for (int num : mp) {
+            if (mp.find(num - 1) == mp.end()) {
+                int curr = 1;
+                int temp = num;
+
+                while (mp.find(temp + 1) != mp.end()) {
+                    curr++;
+                    temp++;
+                }
+                ans = max(ans, curr);
+            }
+        }
+
+        return ans;
+    }
+};
