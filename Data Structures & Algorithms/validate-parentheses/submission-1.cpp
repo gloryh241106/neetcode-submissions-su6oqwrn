ee2239ae248f405df04855a310ad0e60775073cc
Nguyen Huy Hoang <nhhoang2435@clc.fitus.edu.vn>
2026-05-27 21:37:20 +0700

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.size() % 2 == 1)
+            return false;
+
+        stack<char> st;
+        for (char c : s) {
+            if (c == '(')
+                st.push(')');
+            else if (c == '[')
+                st.push(']');
+            else if (c == '{')
+                st.push('}');
+            else {
+                if (st.empty() || st.top() != c)
+                    return false;
+
+                    st.pop();
+            }
+        }
+
+        return st.empty();
+    }
+};
